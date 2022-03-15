090fc6c206104088069e24b887ce840a99d14ab2
dozonalx <ukahdozzy@gmail.com>
2022-03-15 08:54:52 -0700

    working

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+/*
+ * File: 0-main.c
+ * Auth: Brennan D Baraban
+ *
+ */
+
+#include "main.h"
+
+/**
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
