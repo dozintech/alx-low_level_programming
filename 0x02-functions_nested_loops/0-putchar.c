630a61bf5e6187ec0fe4fec187b72d76e595fc9a
dozonalx <ukahdozzy@gmail.com>
2022-03-15 08:49:03 -0700

    working

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,21 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- *main - entry point.
- *Description: program starts and ends here.
- *Return: 0 on success.
- */
+  * main - Entry point
+  *
+  * Return: Always 0
+  */
 int main(void)
 {
-char text[9] = "Holberton";
-int i;
-for (i = 0; i < 9; i++)
-{
-_putchar(text[i]);
-}
-_putchar('\n');
-return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
 }
