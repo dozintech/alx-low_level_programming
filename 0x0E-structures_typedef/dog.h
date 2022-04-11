73370b588ee50201e984e7746d161ac29270a624
dozonalx <ukahdozzy@gmail.com>
2022-04-11 15:55:48 -0700

    dog struct complete

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_H
+#define dog_H
+
+/**
+ * struct dog - Struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * description: this is for a dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+#endif
