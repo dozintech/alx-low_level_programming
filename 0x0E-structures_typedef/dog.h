fcc054a1c56568bd3448c37a866060b34081b050
dozonalx <ukahdozzy@gmail.com>
2022-04-11 21:32:54 -0700

    prototype changed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,18 +1,25 @@
-#ifndef dog_H
-#define dog_H
-
+#ifndef DOG_H
+#define DOG_H
 /**
- * struct dog - Struct dog
- * @name: name
- * @age: age
- * @owner: owner
- * description: this is for a dog
+ *struct dog - structure definition of a dog
+ *@name: character string
+ *@age: integer
+ *@owner: character string
  */
-struct dog
+
+typedef struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-};
+	char *owner;
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+char *_strdup(char *str);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+char *_strcpy(char *dest, char*src);
+int _strlen(char *s);
 
 #endif
