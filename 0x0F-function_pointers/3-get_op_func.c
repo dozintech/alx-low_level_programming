ac979e1d4b136353d37965068d910d3b360ecc8e
dozonalx <ukahdozzy@gmail.com>
2022-04-13 20:50:35 -0700

    perform simple operations

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -18,7 +18,11 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
 		i++;
+	}
 	return (ops[i].f);
 }
