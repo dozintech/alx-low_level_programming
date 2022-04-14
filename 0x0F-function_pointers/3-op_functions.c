6d493ad171f920c415fe4cc74f7ba2a3d9c90688
dozonalx <ukahdozzy@gmail.com>
2022-04-13 21:04:19 -0700

    perform simple operations

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,32 +1,38 @@
 #include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
+
 /**
  * op_add - addition of two intergers
  * @a: first interger
  * @b: second interger
  * Return: sum of intergers
  */
+
 int op_add(int a, int b)
 {
-	return (a + b);
+	return ((a) + (b));
 }
+
 /**
  * op_sub - subtracting  two intergers
  * @a: first interger
  * @b: second interger
  * Return: product of operation
  */
+
 int op_sub(int a, int b)
 {
-	return (a - b);
+	return ((a) - (b));
 }
+
 /**
  * op_div -division  two intergers
  * @a: first interger
  * @b: second interger
  * Return: product of operation
  */
+
 int op_div(int a, int b)
 {
 	if (b == 0)
@@ -34,24 +40,28 @@ int op_div(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-	return (a / b);
+	return ((a) / (b));
 }
+
 /**
  * op_mul - multiplying  two intergers
  * @a: first interger
  * @b: second interger
  * Return: product of operation
  */
+
 int op_mul(int a, int b)
 {
-	return (a * b);
+	return ((a) * (b));
 }
+
 /**
  * op_mod -remainderof the division of two intergers
  * @a: first interger
  * @b: second interger
  * Return: product of operation
  */
+
 int op_mod(int a, int b)
 {
 	if (b == 0)
@@ -59,5 +69,5 @@ int op_mod(int a, int b)
 		printf("Error\n");
 		exit(100);
 	}
-	return (a % b);
+	return ((a) % (b));
 }
