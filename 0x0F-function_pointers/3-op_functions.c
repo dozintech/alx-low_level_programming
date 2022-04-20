1a938fcfd6531c65115e7b1a1704c0797802e694
dozonalx <100067688+dozonalx@users.noreply.github.com>
2022-04-20 08:54:13 +0100

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,63 +1,62 @@
 #include "3-calc.h"
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- * op_add - sum of a and b
- * @a: integer to add
- * @b: integer to add
- * Return: sum if succesful
- */
+* op_add - Sum two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The integer of the function associated.
+*/
 
 int op_add(int a, int b)
 {
-	return ((a) + (b));
+	return (a + b);
 }
+
 /**
- * op_sub - difference of a and b
- * @a: integer to subtract
- * @b: integer to subtract
- * Return: difference if succesful
- */
+* op_sub - Sub two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The function associated
+*/
+
 int op_sub(int a, int b)
 {
-	return ((a) - (b));
+	return (a - b);
 }
+
 /**
- * op_mul - multiply a and b
- * @a: integer to multiply
- * @b: integer to multiply
- * Return: multiplication of a and b if succesful
- */
+* op_mul - MUl two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The function associated
+*/
+
 int op_mul(int a, int b)
 {
-	return ((a) * (b));
+	return (a * b);
 }
+
 /**
- * op_div - divides a and b
- * @a: integer to divide
- * @b: integer to divide
- * Return: division if succesful
- */
+* op_div - Div two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The function associated.
+*/
+
 int op_div(int a, int b)
 {
-	if (!b)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	return ((a) / (b));
+	return (a / b);
 }
+
 /**
- * op_mod - Module of a and b
- * @a: integer
- * @b: integer
- * Return: remainder if succesful
- */
+* op_mod - Mod two numbers.
+* @a: The first number.
+* @b: The second number.
+* Return: The function associated
+*/
+
 int op_mod(int a, int b)
 {
-	if (b)
-		return ((a) % (b));
-	printf("Error\n");
-	exit(100);
+	return (a % b);
 }
