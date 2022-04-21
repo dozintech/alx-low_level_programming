6a18e58f1a612de8055a32c3a0bbe5762e775f68
dozonalx <ukahdozzy@gmail.com>
2022-04-21 10:20:46 -0700

    listed prototypes

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,36 +1,31 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-/*
- *File: list.h
- *
- * Description: Header files containing all prototypes and definitions
- * 		and typeswritten in the 0x12-singly_linked_lists directory.
- */
-
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
 /**
- * stuct ist_s - singly linked list
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: lenth of the string
+ * @len: length of the string
  * @next: points to the next node
  *
- * Description: Singly link list node structure
+ * Description: singly linked list node structure
+ * for Holberton project
  */
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-
 } list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
+int _strlen(const char *);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void print(void) __attribute__((constructor));
 
 #endif
-
